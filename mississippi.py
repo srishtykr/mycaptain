08ea052f17fdca08daae442f8773b33c5d485bbd
srishtykr <64857308+srishtykr@users.noreply.github.com>
2020-05-15 16:39:30 +0530

    mycaptain task 2

---
+++ mississippi.py
@@ -0,0 +1,11 @@
+def most_frequent(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+           dict[n] =1
+            
+    return dict
+print(most_frequent('Mississippi'))
---
+++ printing positive numer.py
@@ -0,0 +1,19 @@
+# Python program to print positive Numbers in a List 
+
+# list of numbers 
+list1 = [12,-7,5,64,-14]
+for num in list1:
+  
+    if num >= 0:
+        print(num,end="   ")
+        num+=1
+
+
+        
+list2 = [12,14,-95,3]
+for num in list2:
+  
+    if num >= 0:
+        print(num,end="   ")
+        num+=1
+
